9057cd22911bb88b2f4b5b370bebc7e2b346fec1
josh-reynolds <josh.reynolds@frontier.com>
2023-12-28 16:41:12 -0800

    Chapter 8: Like Father, Like Son - initial setup

---
+++ Chapter_8/program.java
@@ -0,0 +1,26 @@
+// A Little Java, A Few Patterns
+// Chapter 8: Like Father, Like Son
+
+
+// ======================================
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ======================================
+class Main {
+  public static void main(String[] args){
+    System.out.println("-----------------------------");
+  }
+}
