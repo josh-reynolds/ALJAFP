7041b5610b9398363515bce43582597c561b01d0
josh-reynolds <josh.reynolds@frontier.com>
2024-01-03 09:53:58 -0800

    Chapter 9: Be a Good Visitor - initial setup

---
+++ Chapter_9/program.java
@@ -0,0 +1,15 @@
+// A Little Java, A Few Patterns
+// Chapter 9: Be a Good Visitor
+
+
+// ======================================
+
+// ======================================
+class Main {
+  public static void main(String[] args){
+    System.out.println("-----------------------------");
+    //ExprD e1 = new Plus(new Const(3), new Const(2));
+    //System.out.println(e1);
+    //System.out.println(e1.accept(new IntEvalV()));
+  }
+}
