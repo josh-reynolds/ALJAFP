1c95e41c4d6a944e68646a2b3953cfe8ff419d56
josh-reynolds <josh.reynolds@frontier.com>
2024-01-05 15:26:38 -0800

    Chapter 9: Be a Good Visitor - through p. 147

--- Chapter_9/program.java
+++ Chapter_9/program.java
@@ -91,6 +91,87 @@ class ShadowedCartesianPt extends CartesianPt {
 }
 
 // ======================================
+abstract class ShapeD {
+  abstract boolean accept(ShapeVisitorI _ask);
+}
+
+class Circle extends ShapeD {
+  int r;
+
+  Circle(int _r){
+    r = _r;
+  }
+  //-------------------------------
+  boolean accept(ShapeVisitorI _ask){
+    return _ask.forCircle(r);
+  }
+
+  public String toString(){
+    return "new " + getClass().getName() + "(" + r + ")";
+  }
+}
+
+class Square extends ShapeD {
+  int s;
+
+  Square(int _s){
+    s = _s;
+  }
+  //-------------------------------
+  boolean accept(ShapeVisitorI _ask){
+    return _ask.forSquare(s);
+  }
+
+  public String toString(){
+    return "new " + getClass().getName() + "(" + s + ")";
+  }
+}
+
+class Trans extends ShapeD {
+  PointD q;
+  ShapeD s;
+
+  Trans(PointD _q, ShapeD _s){
+    q = _q;
+    s = _s;
+  }
+  //-------------------------------
+  boolean accept(ShapeVisitorI _ask){
+    return _ask.forTrans(q, s);
+  }
+
+  public String toString(){
+    return "new " + getClass().getName() + "(" + q + ", " + s + ")";
+  }
+}
+
+interface ShapeVisitorI {
+  boolean forCircle(int _r);
+  boolean forSquare(int _s);
+  boolean forTrans(PointD _q, ShapeD _s);
+}
+
+class HasPtV implements ShapeVisitorI {
+  PointD p;
+
+  HasPtV(PointD _p){
+    p = _p;
+  }
+  //-------------------------------
+  public boolean forCircle(int _r){
+    return p.distanceToO() <= _r;
+  }
+
+  public boolean forSquare(int _s){
+    return (p.x <= _s) && (p.y <= _s);
+  }
+
+  public boolean forTrans(PointD _q, ShapeD _s){
+    return _s.accept(new HasPtV(p.minus(_q)));
+  }
+}
+
+// ======================================
 class Main {
   public static void main(String[] args){
     System.out.println("-----------------------------");
@@ -107,5 +188,31 @@ class Main {
     ShadowedCartesianPt s2 = new ShadowedCartesianPt(12, 5, 3, 4);
     System.out.println(s2);
     System.out.println(s2.distanceToO());
+
+    System.out.println("-----------------------------");
+    System.out.println(new CartesianPt(3, 4).closerToO(new ShadowedCartesianPt(1, 5, 1, 2)));
+
+    System.out.println("-----------------------------");
+    Trans t1 = new Trans(new CartesianPt(5, 6), new Circle(10));
+    System.out.println(t1);
+
+    System.out.println("-----------------------------");
+    Trans t2 = new Trans(new CartesianPt(5, 6), new Square(10));
+    System.out.println(t2);
+
+    System.out.println("-----------------------------");
+    Circle cx1 = new Circle(10);
+    System.out.println(cx1);
+    System.out.println(cx1.accept(new HasPtV(new CartesianPt(10, 10))));
+
+    System.out.println("-----------------------------");
+    Square sq1 = new Square(10);
+    System.out.println(sq1);
+    System.out.println(sq1.accept(new HasPtV(new CartesianPt(10, 10))));
+
+    System.out.println("-----------------------------");
+    Trans t3 = new Trans(new CartesianPt(5, 6), new Circle(10));
+    System.out.println(t3);
+    System.out.println(t3.accept(new HasPtV(new CartesianPt(10, 10))));
   }
 }
